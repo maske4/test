4d1fa41a9b7c38acf60ff523e464b0c5555fff64
maske4 <cagin20045456@gmail.com>
2024-05-31 23:40:47 +0300

    Update Bot.js

--- data/Bot.js
+++ data/Bot.js
@@ -1,7 +1,7 @@
 const BotData = {
-    token: "",
-    owners: [""],
-    clientID: ""
+    token: "MTI0NjE4MzcyMTcyMDIxNzYxMA.GydDYE.ArCrj6TTyObd4GhBzYJbXEd2XSdHTxxBuK2uc4",
+    owners: ["818564577536507943"],
+    clientID: "1246183721720217610"
 }
 
 module.exports = { BotData }
